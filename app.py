b2db6d49efed18e6d5eec0b7b98ccdd042e59bbd
Bill Ni <bni@stuy.edu>
2018-11-28 14:19:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,51 @@
-
 import json
 import random
 
-import urllib.request
+from urllib import request, parse, error
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 def load(thing):
-    return json.loads(urllib.request.urlopen(thing).read())
+    return json.loads(request.urlopen(thing).read())
 
 key_loc = "http://homer.stuy.edu/~jtung/sd_keys/p01.json"
 
 @app.route('/')
 def hello_world():
     my_dict = load(key_loc)
-    for k,v in my_dict.items():
-        print (k,v)
-    return 0
+    print(my_dict)
+
+    # time/date
+
+    #requrl = "https://www.calendarindex.com/api/v1/holidays/json?country=US&year=2018&state=NY&api_key="
+    #requrl += my_dict["calendarindex"]
+
+    #print(requrl)
+
+    #d = load(requrl)
+
+    # news
+
+    requrl = "https://newsapi.org/v2/top-headlines?"
+    data = {}
+    data["country"] = "us"
+    data["apiKey"] = my_dict["newsapi"]
+
+    requrl += parse.urlencode(data)
+
+    d = load(requrl)
+    print(d)
+
+    newsarticles = []
+    for i in range(d["totalResults"] - 1):
+        print(i)
+        newsarticles.append([d["articles"][i]["url"], d["articles"][i]["title"]])
+
+    print(newsarticles)
+
+    return render_template("index.html",title="project almanac")
 
 if __name__ == "__main__":
     app.debug = True
