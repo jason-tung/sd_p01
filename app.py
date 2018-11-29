1af25ee6ebc11af2f598cd37e2041d0277be4d76
Bill Ni <bni@stuy.edu>
2018-11-28 20:14:13 -0500

    added news, location and sunrise/sunset apis

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
 import random
+import datetime
 
 from urllib import request, parse, error
 
@@ -15,16 +16,21 @@ key_loc = "http://homer.stuy.edu/~jtung/sd_keys/p01.json"
 @app.route('/')
 def hello_world():
     my_dict = load(key_loc)
-    print(my_dict)
 
     # time/date
 
-    #requrl = "https://www.calendarindex.com/api/v1/holidays/json?country=US&year=2018&state=NY&api_key="
-    #requrl += my_dict["calendarindex"]
+    # requrl = "https://www.calendarindex.com/api/v1/holidays/json?"
+    # data = {}
+    # data["country"] = "US"
+    # data["year"] = "2018"
+    # data["state"] = "NY"
+    # data["api_key"] = my_dict["calendarindex"]
 
-    #print(requrl)
+    # requrl += parse.urlencode(data)
 
-    #d = load(requrl)
+    # d = load(requrl)
+
+    # print(d)
 
     # news
 
@@ -36,14 +42,61 @@ def hello_world():
     requrl += parse.urlencode(data)
 
     d = load(requrl)
-    print(d)
 
     newsarticles = []
-    for i in range(d["totalResults"] - 1):
-        print(i)
-        newsarticles.append([d["articles"][i]["url"], d["articles"][i]["title"]])
+    for i in range(10):
+        newsarticles.append([d["articles"][i]["url"], d["articles"][i]["title"], d["articles"][i]["content"]])
 
-    print(newsarticles)
+    # location
+
+    requrl = "https://ipapi.co/json/"
+    d = load(requrl)
+    lon = d["longitude"]
+    lat = d["latitude"]
+
+    # weather
+
+    requrl = "https://api.darksky.net/forecast/" + my_dict["darksky"] + "/" + str(lat) + "," + str(lon)
+    d = load(requrl)
+
+    weatheralerts = []
+    if "alerts" in d.keys():
+    	for alert in d["alerts"]:
+    		weatheralerts.append([alert["title"], alert["uri"]])
+    currentweather = d["minutely"]["summary"]
+    weekweather = d["daily"]["summary"]
+
+    # sunrise/sunset (today)
+
+    requrl = "https://api.sunrise-sunset.org/json?"
+    data = {}
+    data["lat"] = lat
+    data["lng"] = lon
+    data["date"] = datetime.date.today()
+    data["formatted"] = 0
+
+    requrl += parse.urlencode(data)
+
+    d = load(requrl)
+
+    srisetoday = d["results"]["sunrise"]
+    ssettoday = d["results"]["sunset"]
+
+    # sunrise/sunset (tomorrow)
+
+    requrl = "https://api.sunrise-sunset.org/json?"
+    data = {}
+    data["lat"] = lat
+    data["lng"] = lon
+    data["date"] = datetime.date.today() + datetime.timedelta(days=1)
+    data["formatted"] = 0
+
+    requrl += parse.urlencode(data)
+
+    d = load(requrl)
+
+    srisetmw = d["results"]["sunrise"]
+    ssettmw = d["results"]["sunset"]
 
     return render_template("index.html",title="project almanac")
 
